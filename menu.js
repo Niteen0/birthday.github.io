32e246fd70292fc8c1391b8803df47d84441665e
Niteen kumar <kumarniteen019@gmail.com>
2021-07-10 11:28:51 +0530

    for bday

---
+++ menu.js
@@ -0,0 +1,10 @@
+  var rightnavs = document.getElementById("rightnavs");
+
+   function showmenu(){
+         rightnavs.style.right= "0";
+
+     }
+     function hidemenu(){
+            rightnavs.style.right= "-200px";
+            
+     }
